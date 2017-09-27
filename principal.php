f0f94d326b4609d9326d42c284fd52f7ebc2a92d
kbmendez <31286623+kbmendez@users.noreply.github.com>
2017-09-27 13:56:46 -0500

    principal
    menu de usuarios

---
+++ principal.php
@@ -0,0 +1,81 @@
+<?php
+    include("conexion.php");
+    require ("menu.html");
+?>
+<meta charset="utf-8">
+	<title>D i r e c t o r</title>
+	<link rel="stylesheet" href="css/menu.css">
+	<title></title>
+  <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Blanco</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le styles -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/bootstrap-responsive.css" rel="stylesheet">
+    <link href="css/docs.css" rel="stylesheet">
+    <link href="js/google-code-prettify/prettify.css" rel="stylesheet">
+    <script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
+  <script src="js/jquery.js"></script>
+    <script src="js/bootstrap-transition.js"></script>
+    <script src="js/bootstrap-alert.js"></script>
+    <script src="js/bootstrap-modal.js"></script>
+    <script src="js/bootstrap-dropdown.js"></script>
+    <script src="js/bootstrap-scrollspy.js"></script>
+    <script src="js/bootstrap-tab.js"></script>
+    <script src="js/bootstrap-tooltip.js"></script>
+    <script src="js/bootstrap-popover.js"></script>
+    <script src="js/bootstrap-button.js"></script>
+    <script src="js/bootstrap-collapse.js"></script>
+    <script src="js/bootstrap-carousel.js"></script>
+    <script src="js/bootstrap-typeahead.js"></script>
+    <script src="js/bootstrap-affix.js"></script>
+    <script src="js/holder/holder.js"></script>
+    <script src="js/google-code-prettify/prettify.js"></script>
+    <script src="js/application.js"></script>
+
+    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="assets/js/html5shiv.js"></script>
+    <![endif]-->
+
+    <!-- Le fav and touch icons -->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">
+    <link rel="shortcut icon" href="assets/ico/favicon.png">
+
+     <table class="table table-bordered">
+  <tr class="info">
+    <td>
+      <h3 class="text-info"><img src="imagenes/admin.jpg" class="img-circle" width="80" height="80"> 
+        Bienvenido Director/a:  </h3>
+    </td>
+  </tr>
+</table>
+    <div class="row-fluid" align="center">
+        <div class="span4">
+          <h3 align="center">Profesores</h3>
+            <img src="imagenes/intro.png" style="width: 200px; height: 200px;" title="Profesores"><br>
+            <h3>Registrados:</h3><br>
+            <a href="" class="btn btn-large btn-block btn-primary" type="button"><strong>Ver Mas Informacion</strong></a>
+        </div>
+        <div class="span4">
+          <h3 align="center">Materias</h3>
+            <img src="imagenes/intro.png" style="width: 200px; height: 200px;" title="Materias"><br>
+            <h3>Registradas:</h3><br>
+            <a href="" class="btn btn-large btn-block btn-primary" type="button"><strong>Ver Mas Informacion</strong></a>
+        </div>
+        <div class="span4">
+          <h3 align="center">Alumnos</h3>
+            <img src="imagenes/intro.png" style="width: 200px; height: 200px;" title="Alumnos"><br>
+            <h3>Registrados: </h3><br>
+            <a href="" class="btn btn-large btn-block btn-primary" type="button"><strong>Ver Mas Informacion</strong></a>
+        </div>
+    </div>
